bc757ff37232f9813ebcda03e77d05f93a665fcc
Pasek Asmarajaya <kpasek26@gmail.com>
2016-07-28 19:14:35 +0800

    Add files via upload

---
+++ CodeBali/2015/Crypto/Cipher/cipher.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+with open('cipher.txt','r') as f:
+    numbers = f.read().split(' ')[:-1]
+
+flag = ''
+
+for number in numbers:
+    if len(number) > 4:
+        flag += chr(int(number, 2))
+    elif number[0] == '0':
+        flag += chr(int(number, 8))
+    else:
+        try:
+            flag += chr(int(number))
+        except:
+            flag += chr(int(number, 16))
+
+print (flag)
